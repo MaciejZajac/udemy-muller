7cb3c3f79bd35d11ba94564162ab580dba854c6d
Maciej Zając <maciej.zajac.197@gmail.com>
2020-03-04 22:55:34 +0100

    burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const Burger = ({ ingredients }) => {
+  const transformedIngredients = Object.keys(ingredients).map(igKey => {
+    return [...Array(ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient key={igKey + i} type={igKey} />
+    ));
+  });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
